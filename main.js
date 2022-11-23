39181c70eeaa1dc6c4a1a15d1298f0c8e9bac03a
Aleksander <105987428+majalex@users.noreply.github.com>
2022-11-23 17:07:29 +0100

    second commit

--- main.js
+++ main.js
@@ -6,20 +6,16 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 window.addEventListener("scroll", function(){
-  var header = document.querySelector("header");
+  let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 50)
 });
 
 
 function openTab(tabName) {
-  var i;
-  var x = document.getElementsByClassName("tabcontent");
+  let i;
+  let x = document.getElementsByClassName("tabcontent");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
   document.getElementById(tabName).style.display = "block";
-}
-
-window.onload = function() {
-  document.getElementById("Instagram").focus();
 }
